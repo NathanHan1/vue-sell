e0048b45a18f4e6da716a9f261cd07324c374d65
hanzhibang001 <354172676@qq.com>
2018-05-14 20:20:41 +0800

    second

--- prod.server.js
+++ prod.server.js
@@ -28,7 +28,12 @@ apiRoutes.get('/seller', function (req, res) {
 	});
 });
 
-apiRoutes.get('/goods', function(req,res) {})
+apiRoutes.get('/goods', function (req,res) {
+	res.json({
+		errno: 0,
+		data: goods
+	});
+});
 
 apiRoutes.get('/ratings', function (req, res) {
 	res.json({
